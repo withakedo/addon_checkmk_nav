f8d3eb2ca19c44c90c91922a82485c432a07c864
withakedo <withakedo@gmail.com>
2026-02-09 11:23:38 +0100

    first commit

---
+++ content.js
@@ -0,0 +1,19 @@
+(function() {
+    if (window.self !== window.top) return;
+
+    const currentUrl = window.location.href;
+    const nakedScripts = /(dashboard|view|wato|graph_explorer|availability|discovery|conf)\.py/;
+
+    if (nakedScripts.test(currentUrl) && !currentUrl.includes('index.py')) {
+        
+        const match = currentUrl.match(/(.*\/check_mk\/)(.*)/);
+
+        if (match && match.length === 3) {
+            const baseUrl = match[1] + "index.py";
+            const startUrl = match[2];
+            const finalUrl = `${baseUrl}?start_url=${encodeURIComponent(startUrl)}`;
+            
+            window.location.replace(finalUrl);
+        }
+    }
+})();
